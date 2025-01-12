8a84d778dfd004d0f0646f690f6a7693b373b9f9
branow <branow.code@gmail.com>
2025-01-12 15:09:39 +0200

    Resolve rebasing conflict

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+import { BsTelephoneInboundFill } from 'react-icons/bs'
+import './App.css'
+
+const App: FC = () => {
+  return (
+    <div>
+      <BsTelephoneInboundFill />
+      <span>WEBPHONE</span>
+    </div>
+  )
+}
+
+export default App
