b96e3e6fa14a38dcb768e35fd6c08b36825868c9
branow <branow.code@gmail.com>
2025-01-23 14:24:15 +0200

    Update TextInput to accept className param

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import { IconType } from "react-icons";
 
 interface Props {
+  className?: string;
   label?: string;
   Icon?: IconType;
   name?: string;
@@ -9,13 +10,19 @@ interface Props {
   onChange?: React.ChangeEventHandler<HTMLInputElement>;
 }
 
-const TextInput: FC<Props> = ({ Icon, label, name, value, onChange }) => {
+const TextInput: FC<Props> = ({ className, Icon, label, name, value, onChange }) => {
   return (
     <div>
       {label && (<label htmlFor={name}>{label}</label>)}
       <div>
         {Icon && (<Icon />)}
-        <input id={name} name={name} value={value} onChange={onChange}/>
+        <input
+          className={className}
+          id={name}
+          name={name}
+          value={value}
+          onChange={onChange}
+        />
       </div>
     </div>
   );
