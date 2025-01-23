46856025ea6dcf7f5fa876bcc5e9fa514a3fd8a0
branow <branow.code@gmail.com>
2025-01-23 15:35:19 +0200

    Update ErrorMessage to accept className property

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,13 +1,14 @@
 import { FC } from "react";
 
 interface Props {
+  className?: string;
   error: string;
 }
 
-const ErrorMessage: FC<Props> = ({ error }) => {
+const ErrorMessage: FC<Props> = ({ className, error }) => {
   return (
     <>
-      {error && (<div>{error}</div>)}
+      {error && (<div className={className}>{error}</div>)}
     </>
   );
 };
