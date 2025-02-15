5880a5a835453441d64be4dd22bd62ef5a07f5a6
branow <branow.code@gmail.com>
2025-02-15 15:14:42 +0200

    add React Query for efficient backend communication

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { FC } from 'react'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import PhoneContainer from './components/PhoneContainer'
 import Phone from './components/Phone'
 import './App.css'
 
+const queryClient = new QueryClient();
+
 const App: FC = () => {
   return (
-    <div className="app">
-      <PhoneContainer>
-        <Phone />
-      </PhoneContainer>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="app">
+        <PhoneContainer>
+          <Phone />
+        </PhoneContainer>
+      </div>
+    </QueryClientProvider>
   )
 }
 
