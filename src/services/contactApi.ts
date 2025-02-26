7312c997916ac264c183647bc6272a8aa6f7969d
branow <branow.code@gmail.com>
2025-02-26 10:33:40 +0200

    Modify Contact API to include ID in contact number and add save and delete methods

--- src/services/contactApi.ts
+++ src/services/contactApi.ts
@@ -5,6 +5,7 @@ export enum NumberType {
 }
 
 export interface Number {
+  id: string;
   type: NumberType;
   number: string;
 }
@@ -17,12 +18,23 @@ export interface Contact {
   bio: string;
 }
 
+export enum ContactQueryKeys {
+  CONTACTS = "contacts",
+}
+
 interface Data {
   contacts: Contact[];
 }
 
+let contacts: Contact[] | null = null;
+
 export async function fetchContacts(): Promise<Contact[]> {
-  console.log('fetching contacts');
+  console.log('Fetching contacts');
+
+  if (contacts) {
+    return contacts;
+  }
+
   const response = await fetch('/data.json');
 
   if (!response.ok) {
@@ -30,6 +42,72 @@ export async function fetchContacts(): Promise<Contact[]> {
   }
 
   const data: Data = await response.json();
+  contacts = data.contacts;
   return data.contacts;
 }
 
+export interface SaveContact {
+  id?: number;
+  name: string;
+  numbers: Number[];
+  photo: string;
+  bio: string;
+}
+
+export async function saveContact(contact: SaveContact): Promise<Contact> {
+  return contact.id ? createContact(contact) : updateContact(contact);
+}
+
+async function createContact(contact: SaveContact): Promise<Contact> {
+  console.log("Creating contact");
+
+  const newContact = {
+    id: Math.round(Math.random() * 1000),
+    name: contact.name,
+    numbers: contact.numbers,
+    photo: contact.photo,
+    bio: contact.bio,
+  };
+  return new Promise((resolve, _reject) => {
+    setTimeout(() => {
+      // _reject(new Error("Some error occurs during creation"));
+      contacts!.push(newContact);
+      resolve(newContact);
+    }, 1000);
+  })
+}
+
+async function updateContact(contact: SaveContact): Promise<Contact> {
+  console.log("Updating contact");
+
+  const newContact = {
+    id: contact.id!,
+    name: contact.name,
+    numbers: contact.numbers,
+    photo: contact.photo,
+    bio: contact.bio,
+  };
+  return new Promise((resolve, _reject) => {
+    setTimeout(() => {
+      contacts = contacts!.map(c => {
+        if (c.id === newContact.id) {
+          return contact as Contact;
+        } else {
+          return c;
+        }
+      })
+      resolve(newContact);
+    }, 1000);
+  })
+}
+
+export async function deleteContact(id: number): Promise<boolean> {
+  console.log("Deleting contact");
+
+  return new Promise((resolve, _reject) => {
+    setTimeout(() => {
+      contacts = contacts!.filter(contact => contact.id !== id);
+      resolve(true);
+    }, 1000);
+  })
+}
---
+++ src/util/identifier.ts
@@ -0,0 +1,3 @@
+export function hex(n: number) {
+  return Math.random().toString(16).slice(2, 2 + n);
+}
