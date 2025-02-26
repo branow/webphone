fee3640eb279904d85aa39e9858cb2f4cbdda0b6
branow <branow.code@gmail.com>
2025-02-26 23:40:27 +0200

    Create NumberTypeIcon component

--- src/pages/contacts/ContactNumbers.tsx
+++ src/pages/contacts/ContactNumbers.tsx
@@ -1,9 +1,8 @@
-import { FC, ReactNode } from "react";
-import { IoHomeSharp } from "react-icons/io5";
-import { ImMobile, ImPhone } from "react-icons/im";
-import { MdOutlineWork } from "react-icons/md";
+import { FC } from "react";
+import {ImPhone } from "react-icons/im";
+import NumberTypeIcon from "../../components/NumberTypeIcon";
 import Button from "../../components/Button";
-import { Number, NumberType } from "../../services/contacts";
+import { Number } from "../../services/contacts";
 import { formatPhoneNumber } from "../../util/format";
 import "./ContactNumbers.css";
 
@@ -13,21 +12,13 @@ interface Props {
 }
 
 const ContactNumbers: FC<Props> = ({ numbers, call }) => {
-  const getNumberTypeIcon = (type: NumberType): ReactNode => {
-    switch(type) {
-      case NumberType.HOME: return <IoHomeSharp />;
-      case NumberType.WORK: return <MdOutlineWork />;
-      case NumberType.MOBILE: return <ImMobile />;
-    }
-  }
-
   return (
     <div className="contact-numbers">
       {numbers.map(number => (
         <div className="contact-numbers-number" key={number.number}>
           <div className="contact-numbers-number-type">
             <div className="contact-numbers-number-type-icon">
-              {getNumberTypeIcon(number.type)}
+              <NumberTypeIcon type={number.type} />
             </div>
             <div className="contact-numbers-number-type-lable">
               {number.type}
