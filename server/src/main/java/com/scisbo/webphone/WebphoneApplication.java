0a4e5ff0005d87882cf9c0ce71e825583be51512
branow <branow.code@gmail.com>
2025-04-14 22:58:17 +0300

    Initialize back end project

---
+++ server/src/main/java/com/scisbo/webphone/WebphoneApplication.java
@@ -0,0 +1,13 @@
+package com.scisbo.webphone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebphoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebphoneApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/scisbo/webphone/WebphoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scisbo.webphone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebphoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
