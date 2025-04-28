78538b9b7ca52f88f82583a907fa5906b39dca60
branow <branow.code@gmail.com>
2025-04-28 22:47:47 +0300

    test(log): add tests for SpEL logger feature

---
+++ server/src/test/java/com/scisbo/webphone/log/core/SpelLoggerTest.java
@@ -0,0 +1,104 @@
+package com.scisbo.webphone.log.core;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.event.Level;
+import org.slf4j.spi.LoggingEventBuilder;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.Expression;
+import org.springframework.expression.common.TemplateParserContext;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import com.scisbo.webphone.log.config.LoggingConfiguration;
+import com.scisbo.webphone.log.id.LogIdProvider;
+
+
+@SpringJUnitConfig
+public class SpelLoggerTest {
+
+    @MockitoBean
+    private EvaluationContext context;
+
+    @MockitoBean
+    private LoggingConfiguration config;
+
+    @MockitoBean
+    private SpelExpressionParser parser;
+
+
+    @Test
+    public void testLog() {
+        var spel = "SpEL message";
+        var level = Level.INFO;
+        var message = "message to log";
+        var id = "123";
+
+        Expression expression = mock(Expression.class);
+
+        when(this.parser.parseExpression(eq(spel), any(TemplateParserContext.class)))
+            .thenReturn(expression);
+        when(expression.getValue(this.context, String.class)).thenReturn(message);
+
+        LogIdProvider idProvider = mock(LogIdProvider.class);
+        when(this.config.getLogIdProvider()).thenReturn(idProvider);
+        when(idProvider.getId()).thenReturn(id);
+
+        try (MockedStatic<LoggerFactory> factory = mockStatic(LoggerFactory.class)) {
+            Logger logger = mock(Logger.class);
+            factory.when(() -> LoggerFactory.getLogger(getClass()))
+                .thenReturn(logger);
+
+            LoggingEventBuilder builder = mock(LoggingEventBuilder.class);
+            when(logger.atLevel(level)).thenReturn(builder);
+
+            new SpelLogger(getClass(), context, config, parser).log(level, spel);
+
+            verify(builder).log("[123] " + message);
+        }
+
+    }
+
+    @Test
+    public void testLog_withEmptyId() {
+        var spel = "SpEL message";
+        var level = Level.INFO;
+        var message = "message to log";
+        var id = "";
+
+        Expression expression = mock(Expression.class);
+
+        when(this.parser.parseExpression(eq(spel), any(TemplateParserContext.class)))
+            .thenReturn(expression);
+        when(expression.getValue(this.context, String.class)).thenReturn(message);
+
+        LogIdProvider idProvider = mock(LogIdProvider.class);
+        when(this.config.getLogIdProvider()).thenReturn(idProvider);
+        when(idProvider.getId()).thenReturn(id);
+
+        try (MockedStatic<LoggerFactory> factory = mockStatic(LoggerFactory.class)) {
+            Logger logger = mock(Logger.class);
+            factory.when(() -> LoggerFactory.getLogger(getClass()))
+                .thenReturn(logger);
+
+            LoggingEventBuilder builder = mock(LoggingEventBuilder.class);
+            when(logger.atLevel(level)).thenReturn(builder);
+
+            new SpelLogger(getClass(), context, config, parser).log(level, spel);
+
+            verify(builder).log(message);
+        }
+
+    }
+
+}
