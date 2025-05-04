4beb809a486fb8774fd51b86829211b4102bb1f8
branow <branow.code@gmail.com>
2025-05-04 19:29:59 +0300

    feat(api): add API module for photos

--- client/src/components/Photo.tsx
+++ client/src/components/Photo.tsx
@@ -1,22 +1,50 @@
 import { FC } from "react";
 import { BsPersonFill } from "react-icons/bs";
+import { useQuery } from "@tanstack/react-query";
+import PendingTab from "./PendingTab";
+import ErrorMessage from "./ErrorMessage";
+import PhotoApi from "../services/photos";
 import "./Photo.css";
 
 interface Props {
-  src: string | undefined;
-  size: string;
+  photo?: string;
+  size: number;
   alt?: string;
 }
 
-const Photo: FC<Props> = ({ src, size, alt }) => {
+const Photo: FC<Props> = ({ photo, size, alt }) => {
+  if (!photo) {
+    return (
+      <div className="photo" style={{ width: size, height: size }}>
+        <BsPersonFill size={size - 10}/>
+      </div>
+    )
+  }
+
+  if (isUrl(photo)) {
+    return (
+      <div className="photo" style={{ width: size, height: size }}>
+        <img className="photo-img" src={photo} width={size} alt={alt} />
+      </div>
+    )
+  }
+
+  const fetching = useQuery({
+    queryKey: PhotoApi.QueryKeys.photo(photo),
+    queryFn: () => PhotoApi.get(photo),
+  })
+
   return (
-    <div className="photo">
-      { src ? 
-        (<img className="photo-img" src={src} width={size} alt={alt} />) :
-        (<BsPersonFill size={size}/>)
-      }
+    <div className="photo" style={{ width: size, height: size }}>
+      {fetching.isPending && <PendingTab text="LOADING" /> }
+      {fetching.isError && <ErrorMessage error={fetching.error} />}
+      {fetching.data && <img className="photo-img" src={fetching.data} width={size} alt={alt} />}
     </div>
   );
+}
+
+function isUrl(photo: string): boolean {
+  return photo.includes("http://") || photo.includes("https://");
 }
 
 export default Photo;
---
+++ client/src/services/photos.ts
@@ -0,0 +1,42 @@
+import { RequestBuilder, BACKEND_ORIGIN, handleApiError, logRequestResponse } from "./backend";
+import { ensureAuthentication } from "./keycloak";
+
+export type Photo = {
+  id: string;
+}
+
+export const QueryKeys = {
+  photo: (id: string): string[] => ["photo", id],
+}
+
+export async function get(photo: string): Promise<string> {
+  const { token } = await ensureAuthentication();
+  const response = await new RequestBuilder()
+    .url(u => u.origin(BACKEND_ORIGIN).path(`/api/photos/${photo}`))
+    .get().bearer(token).fetch();
+  return await response
+    .any(logRequestResponse)
+    .error(handleApiError)
+    .handle((res) => res.blobUrl())
+}
+
+export async function upload(photo: File): Promise<Photo> {
+  const { token } = await ensureAuthentication();
+
+  const formData = new FormData();
+  formData.append("photo", photo);
+
+  const response = await new RequestBuilder()
+    .url(u => u.origin(BACKEND_ORIGIN).path("/api/photos"))
+    .post().bearer(token).body(formData).fetch();
+  return await response
+    .any(logRequestResponse)
+    .error(handleApiError)
+    .handle((res) => res.json<Photo>())
+}
+
+export default {
+  QueryKeys,
+  get,
+  upload,
+}
