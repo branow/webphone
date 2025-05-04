ca179ba928cb439129adef62ffc195b3e028508c
branow <branow.code@gmail.com>
2025-05-04 19:25:14 +0300

    feat(env): add environment variable support to client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: './',
-  plugins: [react()],
+export default defineConfig(() => {
+  const expose = [
+    "WEBPHONE_PROFILE",
+    "WEBPHONE_BACKEND_ORIGIN",
+    "WEBPHONE_KEYCLOAK_ORIGIN",
+    "WEBPHONE_KEYCLOAK_REALM",
+    "WEBPHONE_KEYCLOAK_CLIEND_ID"
+  ];
+  return {
+    define: expose.reduce((acc, key) => {
+      acc[`import.meta.env.${key}`] = JSON.stringify(process.env[key] || "");
+      return acc;
+    }, {} as Record<string, string>)
+  }
 })
